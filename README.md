a70ce5f5fceaa15340b41d0a91d5c1fac6d5ae89
Trent <trent@mercatureweb.com>
2022-05-23 00:53:30 -0400

    Added supreme court arguments scraper

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 This repository contains the code responsible for scraping from the Supreme Court website
 
+# Schedule https://www.supremecourt.gov/oral_arguments/2022TermCourtCalendar.pdf
+
 - Court calendar
+- Day Call 
 - Argument Audio and Transcripts
 - Opinions of the court
 - Case documents
+
+
+# Scrape argument audio and transcripts (2010 - present)
+Update recent arguments - python3 scrapers/arguments.py 
+Find arguments for specific year - python3 scrapers/arguments.py 2019
+
+
+outputs to data/ directory
+
+# Scrape archived argument transcripts
---
+++ scrapers/arguments.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import json
+
+supreme_court_arguments = []
+
+# scrape from most current
+url = 'https://www.supremecourt.gov/oral_arguments/argument_audio.aspx'
+
+# scrape from specific year (earliest is 2010)
+if len(sys.argv) > 1:
+
+    year = sys.argv[1] 
+    url = 'https://www.supremecourt.gov/oral_arguments/argument_audio/' + year
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+for a in soup.find_all('a'):
+    if 'href' in a.attrs:
+        path = a.get('href')
+        if '../audio' in path:
+
+            path = path.replace("../", "")
+            r = requests.get('https://www.supremecourt.gov/oral_arguments/' + path)
+            soup = BeautifulSoup(r.content, 'html.parser')
+
+            title = soup.find(id="ctl00_ctl00_MainEditable_mainContent_lblCaseName")
+            docket = soup.find(id="ctl00_ctl00_MainEditable_mainContent_lblDocket")
+            date = soup.find(id="ctl00_ctl00_MainEditable_mainContent_lblDate")
+
+            transcript_url = ""
+            audio_url = ""
+
+            for a in soup.find_all('a'):
+                if 'href' in a.attrs:
+                    path = a.get('href')
+
+                    if '.mp3' in path:
+                        audio_url = path  
+                    if 'argument_transcripts' in path:
+                        transcript_url = path 
+
+            supreme_court_arguments.append({
+                'title': title.text, 'date': date.text, 'docket': docket.text, 
+                'audioUrl': audio_url, 'transcriptUrl': transcript_url
+            })
+
+
+if len(sys.argv) > 1:
+    year = sys.argv[1] 
+    with open('data/arguments-' + year + '.json', "w") as outfile:
+        json.dump(supreme_court_arguments, outfile, indent=4)
+else:
+    with open('data/arguments.json', "w") as outfile:
+        json.dump(supreme_court_arguments, outfile, indent=4)
+
+
+
+
